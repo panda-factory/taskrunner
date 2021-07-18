cfd5ed75736af74479b99087dce878606ea370cf
panda-factory <276715287@qq.com>
2021-07-18 18:06:14 +0800

    benchmark

--- benchmark/task_runner_benchmark.cpp
+++ benchmark/task_runner_benchmark.cpp
@@ -4,30 +4,37 @@
 #include "benchmark/benchmark.h"
 #include <memory>
 #include <string>
-static void BM_SHARED_PTR(benchmark::State& state) {  // NOLINT
-    std::shared_ptr<std::string> shard_ptr = std::make_shared<std::string>();
+#include <atomic>
+
+#include "task_runner/task_runner.h"
+#include "task_runner/delayed_task_runner.h"
+void foo()
+{
+    ;
+}
+
+static void BM_TASK_RUNNER(benchmark::State& state) {  // NOLINT
+    auto task_runner = wtf::TaskRunner::Create("task-runner");
+    std::atomic_int count = 0;
     for (auto _ : state) {
         // This code gets timed
-        shard_ptr->empty();
+        task_runner->PostTask([&count]() {
+            count++;
+        });
     }
 }
-static void BM_UNIQUE_PTR(benchmark::State& state) {  // NOLINT
-    std::unique_ptr<std::string> unique_ptr = std::make_unique<std::string>();
+static void BM_DELAYED_TASK_RUNNER(benchmark::State& state) {  // NOLINT
+    auto task_runner = wtf::DelayedTaskRunner::CreateTaskRunner("task-runner");
+    std::atomic_int count = 0;
     for (auto _ : state) {
         // This code gets timed
-        unique_ptr->empty();
-    }
-}
-static void BM_RAW_PTR(benchmark::State& state) {  // NOLINT
-    std::string* raw_ptr = new std::string();
-    for (auto _ : state) {
-        // This code gets timed
-        raw_ptr->empty();
+        task_runner->PostTask([&count]() {
+            count++;
+        });
     }
 }
 
 // Run the benchmark
-BENCHMARK(BM_SHARED_PTR);
-BENCHMARK(BM_UNIQUE_PTR);
-BENCHMARK(BM_RAW_PTR);
+BENCHMARK(BM_TASK_RUNNER);
+BENCHMARK(BM_DELAYED_TASK_RUNNER);
 BENCHMARK_MAIN();
